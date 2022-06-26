4090d093877b4402e9c93b3c4b8238a31b448b4a
Josh Ackland <joshackland98@gmail.com>
2022-06-26 17:42:41 +0800

    Improved MongoDB model - Changed how date of creation is stored

--- model/User.js
+++ model/User.js
@@ -1,32 +1,31 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    min: 6,
-    max: 255,
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      min: 6,
+      max: 255,
+    },
+    email: {
+      type: String,
+      required: true,
+      max: 255,
+      min: 3,
+    },
+    password: {
+      type: String,
+      required: true,
+      max: 4096,
+      min: 8,
+    },
+    admin: {
+      type: Boolean,
+      required: true,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    max: 255,
-    min: 3,
-  },
-  password: {
-    type: String,
-    required: true,
-    max: 4096,
-    min: 8,
-  },
-  date: {
-    type: Date,
-    default: Date.now,
-  },
-  admin: {
-    type: Boolean,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", userSchema);
